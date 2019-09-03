1e21df63f8e1ef94e436eb5ab6ce777f1fbdfcb4
Ryan <artlessavian@gmail.com>
2019-09-02 21:23:28 -0700

    here we go again

--- Game/Modes/BoardDecorators/GarbageMeter.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-#func _ready():
-
-func _process(_delta):
-	for i in range(self.get_child_count()):
-#		if i <= $"..".garbage_inbox:
-		self.get_child(i).visible = i < $"..".garbage_inbox;
